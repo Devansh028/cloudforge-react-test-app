b871b7615b49c7700e96c2b9f2991b8d0dfd8c35
Devansh028 <devanshkumar370@gmail.com>
2026-05-29 18:33:32 +0530

    Initial commit: CloudForge React Test App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="quote-poster">
+      <p className="quote-text">
+        You can speak with spiritual eloquence, pray in public, and maintain a holy appearance... but it is your <span className="italic">behavior</span> that will reveal your true character.
+      </p>
+      <div className="quote-author">
+        - Dr. Steve Maraboli
+      </div>
+    </div>
+  )
+}
+
+export default App
